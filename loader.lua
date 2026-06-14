94a26e396f850a8fd50d0b51379900ad5871ac7b
tracemason1088-hash <tracemason1088@gmail.com>
2026-06-14 17:13:40 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,9 @@
-loadstring(game:HttpGet("https://githubusercontent.com"))()
+-- loader.lua
+local repo = "https://raw.githubusercontent.com/tracemason1088-hash/ROBLOX-SCRIPT/main/"
+
+loadstring(game:HttpGet(repo .. "menu.lua"))()
+loadstring(game:HttpGet(repo .. "esp.lua"))()
+loadstring(game:HttpGet(repo .. "fly.lua"))()
+loadstring(game:HttpGet(repo .. "movement.lua"))()
+
+getgenv().Notify("All modules loaded! Use :help", Color3.fromRGB(255,200,100))
